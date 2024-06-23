ac74d7722396f71b40e258b8f9606b00c104fe10
Luciana <luciana.holzmann@gmail.com>
2024-06-23 02:04:10 +0200

    [CHORE] Configuración testing

--- src/jest.config.cjs
+++ src/jest.config.cjs
@@ -1,4 +1,9 @@
 module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    setupFiles: ['./jest.setup.js']
-}
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/src/test/**/*.test.ts?(x)'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  setupFilesAfterEnv: ['./jest.setup.ts']
+};
