17954e0c9f4126d009e969bd5217458953a0ed4e
Luciana <luciana.holzmann@gmail.com>
2024-06-24 00:46:46 +0200

    [CHORE] Configuración testing

---
+++ jest.setup.cjs
@@ -0,0 +1,3 @@
+// jest.setup.cjs
+import 'whatwg-fetch'; // <-- yarn add whatwg-fetch
+import '@testing-library/jest-dom'; // Importar jest-dom para tener matchers adicionales
--- src/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testMatch: ['**/src/test/**/*.test.ts?(x)'],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-  },
-  setupFilesAfterEnv: ['./jest.setup.ts']
-};
