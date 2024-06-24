cce78387b8a8de73bfc7f0fa109d34084643938f
Luciana <luciana.holzmann@gmail.com>
2024-06-24 21:07:02 +0200

    [CHORE] TypeScript

--- src/08-useReducer/types/todos.types.ts
+++ src/08-useReducer/types/todos.types.ts
@@ -1,34 +1,30 @@
-type Todo = {
+export type Todo = {
   id: number;
   description: string;
   done: boolean;
 };
 
-type Action =
+export type Action =
   | { type: "Add Todo"; payload: Todo }
   | { type: "Remove Todo"; payload: number }
   | { type: "Toggle Todo"; payload: number };
 
-type Props = {
-  initialState: Todo[];
+export type NewTodo = {
+  description: string;
 };
 
-export type NewTodo = {
-    description: string;
-  };
-  
-  export type TodoAddProps = {
-    onNewTodo: (todo: Todo) => void;
-  };
+export type TodoAddProps = {
+  onNewTodo: (todo: Todo) => void;
+};
 
-  type TodoItemProps = {
-    todo: Todo;
-    onDeleteTodo: (id: number) => void;
-    onToggleTodo: (id: number) => void;
-  };
+export type TodoItemProps = {
+  todo: Todo;
+  onDeleteTodo: (id: number) => void;
+  onToggleTodo: (id: number) => void;
+};
 
-  type TodoListProps = {
-    todos: Todo[];
-    onDeleteTodo: (id: number) => void;
-    onToggleTodo: (id: number) => void;
-  };
+export type TodoListProps = {
+  todos: Todo[];
+  onDeleteTodo: (id: number) => void;
+  onToggleTodo: (id: number) => void;
+};
