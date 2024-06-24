51fae1678644382fc9464633268dff8523dee1a6
Luciana <luciana.holzmann@gmail.com>
2024-06-25 00:32:18 +0200

    [CHORE] Configuración testing

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,14 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  testMatch: ['**/src/test/**/*.test.ts?(x)'],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-  },
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest'
-  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
 };
